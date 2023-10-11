06b117fef07567eef3f04477352265f1ccbcfdce
Matiss <matissp1302@gmail.com>
2023-10-11 21:42:13 +0300

    Refactored some of the code

--- src/components/VisualizeArray/VisualizeArray.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./VisualizeArray.css";
-
-function VisualizeArray({ height }) {
-  return <div className="array-col" style={{ height: `${height}px` }}></div>;
-}
-
-export default VisualizeArray;
